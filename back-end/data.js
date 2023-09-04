588d615fa5ba6c61d05aabd7687bfcdbe57b2c87
Asbjoernemil <asbjoernemil24@gmail.com>
2023-09-04 17:55:45 +0200

    Html

---
+++ back-end/data.js
@@ -0,0 +1,102 @@
+[
+    {
+        "name": "Michael Jackson",
+        "birthdate": "August 29, 1958",
+        "activeSince": "1964",
+        "genres": ["Pop", "R&B"],
+        "labels": ["Epic Records"],
+        "website": "https://www.michaeljackson.com/",
+        "image": "michael-jackson.jpg",
+        "shortDescription": "King of Pop known for his iconic music and performances."
+    },
+    {
+        "name": "The Beatles",
+        "birthdate": "Formed in 1960",
+        "activeSince": "1960s",
+        "genres": ["Rock", "Pop"],
+        "labels": ["Apple Records"],
+        "website": "https://www.thebeatles.com/",
+        "image": "the-beatles.jpg",
+        "shortDescription": "Legendary British rock band that revolutionized music."
+    },
+    {
+        "name": "Bob Marley",
+        "birthdate": "February 6, 1945",
+        "activeSince": "1962",
+        "genres": ["Reggae"],
+        "labels": ["Tuff Gong"],
+        "website": "https://www.bobmarley.com/",
+        "image": "bob-marley.jpg",
+        "shortDescription": "Reggae icon and global ambassador for peace."
+    },
+    {
+        "name": "Elvis Presley",
+        "birthdate": "January 8, 1935",
+        "activeSince": "1954",
+        "genres": ["Rock and Roll"],
+        "labels": ["RCA Records"],
+        "website": "https://www.elvis.com/",
+        "image": "elvis-presley.jpg",
+        "shortDescription": "The King of Rock and Roll, known for his charisma and music."
+    },
+    {
+        "name": "Beyoncé",
+        "birthdate": "September 4, 1981",
+        "activeSince": "Late 1990s",
+        "genres": ["R&B", "Pop"],
+        "labels": ["Columbia Records"],
+        "website": "https://www.beyonce.com/",
+        "image": "beyonce.jpg",
+        "shortDescription": "Global superstar and powerful vocalist."
+    },
+    {
+        "name": "Miles Davis",
+        "birthdate": "May 26, 1926",
+        "activeSince": "1940s",
+        "genres": ["Jazz"],
+        "labels": ["Columbia Records"],
+        "website": "https://www.milesdavis.com/",
+        "image": "miles-davis.jpg",
+        "shortDescription": "Influential jazz trumpeter and composer."
+    },
+    {
+        "name": "Madonna",
+        "birthdate": "August 16, 1958",
+        "activeSince": "1980s",
+        "genres": ["Pop", "Dance"],
+        "labels": ["Sire Records"],
+        "website": "https://www.madonna.com/",
+        "image": "madonna.jpg",
+        "shortDescription": "Queen of Pop and cultural icon."
+    },
+    {
+        "name": "Prince",
+        "birthdate": "June 7, 1958",
+        "activeSince": "Late 1970s",
+        "genres": ["Pop", "Funk", "R&B"],
+        "labels": ["Warner Bros. Records"],
+        "website": "https://prince.com/",
+        "image": "prince.jpg",
+        "shortDescription": "Musical genius and iconic performer."
+    },
+    {
+        "name": "Adele",
+        "birthdate": "May 5, 1988",
+        "activeSince": "2000s",
+        "genres": ["Pop", "Soul"],
+        "labels": ["XL Recordings"],
+        "website": "https://www.adele.com/",
+        "image": "adele.jpg",
+        "shortDescription": "Soulful British singer-songwriter with a powerful voice."
+    },
+    {
+        "name": "David Bowie",
+        "birthdate": "January 8, 1947",
+        "activeSince": "1960s",
+        "genres": ["Rock", "Pop"],
+        "labels": ["RCA Records"],
+        "website": "https://www.davidbowie.com/",
+        "image": "david-bowie.jpg",
+        "shortDescription": "Music chameleon and influential rock artist."
+    }
+]
